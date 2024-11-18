a1355b6cde5edd7ca65d9fe4bc380b1c9c0fac6c
karthik-kunjarakana <karthikkunjarakana@gmail.com>
2024-11-19 00:29:58 +0530

    second commit

---
+++ C++ Vectors/c-vec-2.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int> numbers = {1, 2, 3, 4, 5};
+    for(int number:numbers){
+        cout<<number<<endl;
+    } 
+}
