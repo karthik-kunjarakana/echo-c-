2d8d5ff56ff5bd4144d69cac2c7fa468a491029d
karthik-kunjarakana <karthikkunjarakana@gmail.com>
2024-11-21 17:08:02 +0530

    understanding more about vectors

--- C++ Vectors/c-vec-1.cpp
+++ C++ Vectors/c-vec-1.cpp
@@ -1,3 +1,10 @@
+/*A vector in C++ is like a resizable array.
+
+Both vectors and arrays are data structures used to store multiple elements of the same data type.
+
+The difference between an array and a vector, is that the size of an array cannot be modified (you cannot add or remove elements from an array). A vector however, can grow or shrink in size as needed.
+
+To use a vector, you have to include the <vector> header file*/
 #include <iostream>
 #include <vector>
 using namespace std;
---
+++ C++ Vectors/c-vec3.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int> numbers = {1, 2, 3, 4, 5};
+    cout<<numbers[0]<<endl;
+    cout<<numbers.at(1)<<endl;
+    return 0;
+    } 
